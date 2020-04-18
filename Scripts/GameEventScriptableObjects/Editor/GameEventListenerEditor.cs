bec6541b25c794097f8f4e82c133607a4c9bec11
Jack Hoefnagel <jackhoefnagel@gmail.com>
2020-04-18 08:08:33 +0200

    Game Event Listener Scriptable Objects + Editor Script, from Unite Austin 2017 talk

---
+++ Scripts/GameEventScriptableObjects/Editor/GameEventListenerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameEventSO))]
+public class GameEventListenerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GameEventSO myScript = (GameEventSO)target;
+        if (GUILayout.Button("Raise Event"))
+        {
+            myScript.Raise();
+        }
+    }
+}
---
+++ Scripts/GameEventScriptableObjects/GameEventSO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameEvent")]
+public class GameEventSO : ScriptableObject {
+
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise(){
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+
+    public void RegisterListener(GameEventListener listenerToRegister){
+        listeners.Add(listenerToRegister);
+    }
+
+    public void RemoveListener(GameEventListener listenerToRemove)
+    {
+        listeners.Remove(listenerToRemove);
+    }
+}
